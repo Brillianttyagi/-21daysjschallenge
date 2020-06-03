869df0dfe99b9abeb4f20bd7f0f62a8e6145ea1d
Deepanshu tyagi <deepanshubhai84@gmail.com>
2020-06-03 20:15:26 +0530

    Create Day1.js

---
+++ Day1.js
@@ -0,0 +1,11 @@
+ //Day1
+ //Data types and variables
+ /* undefined,null,boolean,string,symbol,number,and object
+ */
+var name = 'John'; //string variable
+var age = 25; //number variable
+var country = 'India';  //string variable
+console.log(name,'is',age,'years old','and lives in',country);
+// Declare multivariable in one statment
+var Firstname ='John',Lastname = 'Doe';
+console.log('Firstname:',Firstname,'Lastname:',Lastname)

f6113d603ce8437ccacc7a6b7a4f8ef5a1ac43c4
Deepanshu tyagi <deepanshubhai84@gmail.com>
2020-06-10 21:09:55 +0530

    Update Day8.js

--- Day8.js
+++ Day8.js
@@ -1,1 +1,33 @@
 
+//Day 8 
+/*Valid Data Types
+In JSON, values must be one of the following data types:
+
+a string
+a number
+an object (JSON object)
+an array
+a boolean
+null
+*/
+//JSON.parse()
+var obj = JSON.parse('{ "name":"John", "age":30, "city":"New York"}');
+
+//JSON.stringify()
+var obj = { name: "John", age: 30, city: "New York" };
+var myJSON = JSON.stringify(obj);
+
+//JASON Objects
+myObj = { "name":"John", "age":30, "car":null };
+x = myObj["name"];
+
+//JASON Arrays
+//Arrays as JSON Objects
+{}
+"name":"John",
+"age":30,
+"cars":[ "Ford", "BMW", "Fiat" ]
+}
+
+//Accessing Array Values
+x = myObj.cars[0];
